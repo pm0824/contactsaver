3d0158da73aa2f235fc57c838dd9dcc6d3d9515f
pm0824 <maneprachi824@gmail.com>
2021-06-26 15:27:42 +0530

    Register, load user and set token

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,12 +2,12 @@ import React, { useState, useContext, useEffect } from 'react'
 import AlertContext from '../../context/alert/alertContext'
 import AuthContext from '../../context/auth/authContext'
 
-const Register = () => {
+const Register = props => {
     const alertContext = useContext(AlertContext)
     const authContext = useContext(AuthContext)
 
     const { setAlert } = alertContext
-    const { register, error, clearErrors } = authContext
+    const { register, error, clearErrors, isAuthenticated } = authContext
 
     const [user, setUser] = useState({
         name: '',
@@ -17,12 +17,15 @@ const Register = () => {
       });
 
     useEffect(()=>{
+        if(isAuthenticated){
+            props.history.push('/')
+        }
         if(error){
             setAlert(error, "danger")
             clearErrors()
         }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[error])
+    },[error,isAuthenticated,props.history])
     
       const { name, email, password, password2 } = user;
     
