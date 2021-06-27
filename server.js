5fae7635c9320b3e56d0a4422df58b5da18920b1
pm0824 <maneprachi824@gmail.com>
2021-06-27 13:59:47 +0530

    Prepare for deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db')
+const path = require('path')
 
 const app = express();
 
@@ -9,11 +10,23 @@ connectDB();
 //Init middleware
 app.use(express.json({extended:false}));        //bodyparser
 
-app.get('/',(req,res)=> res.json({msg:"Hello World!"}));
+//app.get('/',(req,res)=> res.json({msg:"Hello World!"}));
 
+//Define Routes
 app.use('/api/users',require('./routes/users'));
 app.use('/api/contacts',require('./routes/contacts'));
 app.use('/api/auth',require('./routes/auth'));
+
+//Serve static assets in production
+if(process.env.NODE_ENV === 'production'){
+    //Set static folder
+    app.use(express.static('client/build'))
+
+    app.get('*', (req,res)=> res.sendFile(
+        path.resolve(__dirname,'client','build','index.html')
+        )
+    )                                       //app.get(anything) then load index.html in build
+}
 
 const PORT = process.env.PORT || 5000;
 
